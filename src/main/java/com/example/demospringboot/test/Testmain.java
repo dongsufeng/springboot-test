3e53ee853d1b3fed2ff12b97072724bd200d9f48
yalin <1663991989@qq.com>
2017-12-21 14:28:21 +0800

    添加main方法

---
+++ src/main/java/com/example/demospringboot/test/Testmain.java
@@ -0,0 +1,11 @@
+package com.example.demospringboot.test;
+
+/**
+ * @Author:dongsufeng
+ * @Date:Credate in 14:26 2017-12-21
+ */
+public class Testmain {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
