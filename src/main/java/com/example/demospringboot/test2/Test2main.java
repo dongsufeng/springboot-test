32c933c808de80292cb5caf3d4f8996744c5fbf1
yalin <1663991989@qq.com>
2017-12-28 11:29:09 +0800

    test2

---
+++ src/main/java/com/example/demospringboot/test2/Test2main.java
@@ -0,0 +1,11 @@
+package com.example.demospringboot.test2;
+
+/**
+ * @Author:dongsufeng
+ * @Date:Credate in 11:28 2017-12-28
+ */
+public class Test2main {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
