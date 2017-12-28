95f9b2e809373dac7d0fd2eadab46867c0bfca5d
yalin <1663991989@qq.com>
2017-12-28 11:11:36 +0800

    test9

--- src/main/java/com/example/demospringboot/test/Testmain.java
+++ src/main/java/com/example/demospringboot/test/Testmain.java
@@ -12,6 +12,7 @@ public class Testmain {
         System.out.println("4");
         System.out.println("5");
         System.out.println("6");
-        System.out.println("9");
+        System.out.println("7");
+        System.out.println("8");
     }
 }
