ac18c577702968091c2ae430654a61dcf45367f6
yalin <1663991989@qq.com>
2018-01-05 17:55:15 +0800

    testnew

--- src/main/java/com/example/demospringboot/test2/Test2main.java
+++ src/main/java/com/example/demospringboot/test2/Test2main.java
@@ -11,6 +11,6 @@ public class Test2main {
         System.out.println("3");
         System.out.println("4");
         System.out.println("6");
-        System.out.println("7");
+        System.out.println("888");
     }
 }
